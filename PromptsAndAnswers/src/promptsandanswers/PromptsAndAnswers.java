93b4f027a210f6f0353ccc687e99202ce3f8c26e
EDGIES <36707607+EDGIES@users.noreply.github.com>
2018-09-24 15:43:27 -0400

    Add files via upload

---
+++ PromptsAndAnswers/src/promptsandanswers/PromptsAndAnswers.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package promptsandanswers;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author grant.egyed
+ */
+public class PromptsAndAnswers {
+    
+
+   static Map <String,String> mapAnswers = new HashMap<>();
+    
+   public static void main(String[] args) {
+        // TODO code application logic here
+    
+   
+   
+   }
+   
+   public static void Prompts(){
+       System.out.println("Marry went to look at a Dog then went to the store.");
+       
+}
+}
