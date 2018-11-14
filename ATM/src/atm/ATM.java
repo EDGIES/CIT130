79dbbada8bf2a0be485aef418306e6cf43af1661
EDGIES <36707607+EDGIES@users.noreply.github.com>
2018-11-14 15:56:43 -0500

    Add files via upload

---
+++ ATM/src/atm/ATM.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author grant.egyed
+ */
+public class ATM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void CreatingAnAccount(){
+       Account accountHolder = new Account();
+        System.out.println("Please Enter your First Name");
+        Scanner userInputScanner = new Scanner (System.in);
+        accountHolder.
+    }
+    
+}
---
+++ ATM/src/atm/Account.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author grant.egyed
+ */
+public class Account {
+    private int accountNumber;
+    private String accountHolderFirstName;
+    private String accountHolderLastName;
+    private int pin;
+    private int cash;
+}
