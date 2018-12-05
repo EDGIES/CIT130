65274ffe738b5e10945fcb36bb9d2a1ae645c00d
EDGIES <36707607+EDGIES@users.noreply.github.com>
2018-12-05 12:45:28 -0500

    Add files via upload

---
+++ ATMGUI/src/atmgui/FXMLDocumentController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atmgui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author egyedg
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    @FXML
+     private void button1(ActionEvent event) {
+        
+     }//close button1
+     
+     @FXML
+      private void button2(ActionEvent event) {
+        
+     }//close button2
+      
+      @FXML
+       private void button3(ActionEvent event) {
+        
+     }//close button3
+       
+       @FXML
+        private void button4(ActionEvent event) {
+        
+     }//close button4
+        @FXML
+         private void button5(ActionEvent event) {
+        
+     }//close button5
+         @FXML
+          private void button6(ActionEvent event) {
+        
+     }//close button6
+          @FXML
+           private void button7(ActionEvent event) {
+        
+     }//close button7
+           @FXML
+            private void button8(ActionEvent event) {
+        
+     }//close button8
+            @FXML
+             private void button9(ActionEvent event) {
+        
+     }//close button9
+             @FXML
+              private void buttonDecimal(ActionEvent event) {
+        
+     }//close buttonDecimal
+              @FXML
+              private void button0(ActionEvent event){
+                  
+     }//close buuton0
+              
+              @FXML
+              private void button00(ActionEvent event){
+                  
+     }//close button00
+              @FXML
+              private void cancelButton(ActionEvent event){
+                  
+     }//close cancelButton
+              @FXML
+              private void correctButton(ActionEvent event){
+                  
+     }//close correctButton
+              @FXML
+              private void blankButton(ActionEvent event){
+                  
+     }//close blankButton
+               @FXML
+              private void confirmButton(ActionEvent event){
+                  
+     }//close confirmButton
+              
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
